bb5461027788be3c5e394821bfc180fd460504c4
pllocru <pllocru0706@gmail.com>
2025-03-29 13:30:06 +0100

    tailwind 3.4 para vue 3.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { MotionDirective as motion } from '@vueuse/motion'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
