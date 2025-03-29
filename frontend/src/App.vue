613e0cf13946db2028a902ee5e73b97152e1c574
pllocru <pllocru0706@gmail.com>
2025-03-29 13:31:27 +0100

    rutas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,3 @@
-<script setup>
-import AppLayout from './layouts/AppLayout.vue'
-import { ref, onMounted } from 'vue'
-import api from './services/api'
-
-
-const vehiculos = ref([])
-
-onMounted(async () => {
-  try {
-    const res = await api.get('/vehiculos')
-    vehiculos.value = res.data
-  } catch (error) {
-    console.error('Error al conectar con la API:', error)
-  }
-})
-</script>
-
 <template>
-  <AppLayout>
-    <h2 class="text-2xl font-semibold text-center my-4">Animado 🎉</h2>
-    <ul class="text-center space-y-2">
-      <motion v-for="v in vehiculos" :key="v.id" :initial="{ opacity: 0, x: -20 }" :enter="{ opacity: 1, x: 0 }">
-        <li>{{ v.matricula }} - {{ v.marca }} {{ v.modelo }}</li>
-      </motion>
-    </ul>
-  </AppLayout>
+  <router-view />
 </template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { MotionPlugin } from '@vueuse/motion'
+import router from './router'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 import './style.css'
 
-createApp(App).use(MotionPlugin).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(MotionPlugin)
+app.use(Toast, {
+  position: 'top-right',
+  timeout: 3000,
+})
+
+app.mount('#app')
